4062d19ade51df3f9377a8ae0cd350f63477f9a0
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2011-05-31 12:23:39 +0200

    added support for postgresql version 9.0

---
+++ manifests/classes/postgis-debian-v9-0.pp
@@ -0,0 +1,22 @@
+/*
+
+==Class: postgis::debian::v9-0
+
+Requires:
+ - Class["apt::preferences"]
+
+*/
+class postgis::debian::v9-0 inherits postgis::debian::base {
+
+  case $lsbdistcodename {
+    squeeze : { 
+      Package["postgis"] {
+        name => "postgresql-9.0-postgis"
+      }
+    }
+
+    default: {
+      fail "${name} not available for ${operatingsystem}/${lsbdistcodename}"
+    }
+  }
+}
