53169dc88c084902fc2325a65e467ae74700cd62
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2011-05-31 11:43:35 +0200

    postgis: added version 9.0

--- manifests/init.pp
+++ manifests/init.pp
@@ -49,3 +49,16 @@ class postgis::v8-4 {
     default: { notice "Unsupported operatingsystem ${operatingsystem}" }
   }
 }
+
+class postgis::v9-0 {
+  case $operatingsystem {
+    Debian: {
+      case $lsbdistcodename {
+        squeeze : { include postgis::debian::v9-0 }
+        default : { fail "${name} not available for ${operatingsystem}/${lsbdistcodename}"}
+      }
+    }
+    default: { notice "Unsupported operatingsystem ${operatingsystem}" }
+  }
+}
+
