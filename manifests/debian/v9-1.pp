ab27645460c9467e07b2f75432084421ef71c3dd
Frederic Junod <frederic.junod@camptocamp.com>
2012-08-23 15:05:41 +0200

    Add postgresql 9.1 support

---
+++ manifests/debian/v9-1.pp
@@ -0,0 +1,20 @@
+#
+# ==Class: postgis::debian::v9-1
+#
+# Requires:
+#  - Class["apt::preferences"]
+#
+class postgis::debian::v9-1 inherits postgis::debian::base {
+
+  case $lsbdistcodename {
+    squeeze : {
+      Package["postgresql-postgis"] {
+        name => "postgresql-9.1-postgis"
+      }
+    }
+
+    default: {
+      fail "${name} not available for ${operatingsystem}/${lsbdistcodename}"
+    }
+  }
+}
---
+++ manifests/v9-1.pp
@@ -0,0 +1,11 @@
+class postgis::v9-1 {
+  case $operatingsystem {
+    Debian: {
+      case $lsbdistcodename {
+        squeeze : { include postgis::debian::v9-1 }
+        default : { fail "${name} not available for ${operatingsystem}/${lsbdistcodename}"}
+      }
+    }
+    default: { notice "Unsupported operatingsystem ${operatingsystem}" }
+  }
+}
