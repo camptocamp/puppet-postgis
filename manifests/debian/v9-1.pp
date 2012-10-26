c0b83334c423ebeea7ae31386c22d5b12a592190
Frederic Junod <frederic.junod@camptocamp.com>
2012-10-26 15:53:22 +0200

    coding style: simple quote

--- manifests/debian/v9-1.pp
+++ manifests/debian/v9-1.pp
@@ -2,14 +2,14 @@
 # ==Class: postgis::debian::v9-1
 #
 # Requires:
-#  - Class["apt::preferences"]
+#  - Class['apt::preferences']
 #
 class postgis::debian::v9-1 inherits postgis::debian::base {
 
   case $::lsbdistcodename {
     squeeze : {
-      Package["postgresql-postgis"] {
-        name => "postgresql-9.1-postgis"
+      Package['postgresql-postgis'] {
+        name => 'postgresql-9.1-postgis'
       }
     }
 
