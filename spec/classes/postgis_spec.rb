45f339c9ccbbf70f52bd3f865fd6104262e5a467
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2014-09-24 17:26:07 +0200

    Use EXTENSION (new way for spatially-enabled database) for PG>=9.1 & PostGIS>=2.0
     * remove access privileges on postgis_template (not required by default)
     * adapt unit tests

--- spec/classes/postgis_spec.rb
+++ spec/classes/postgis_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 describe 'postgis' do
 
-  let :pre_condition do
-    "class {'postgresql::server':}"
-  end
+  context 'on Debian wheezy with old postgis version' do
 
-  context 'on Debian wheezy' do
+    let :pre_condition do
+      "class {'::postgresql::globals': encoding => 'UTF8', version => '9.1', postgis_version => '1.5'} -> class {'::postgresql::server':}"
+    end
 
     let(:facts) {{
       :concat_basedir           => '/var/lib/puppet/concat',
@@ -15,24 +15,16 @@ describe 'postgis' do
       :operatingsystemrelease   => 'wheezy',
       :osfamily                 => 'Debian',
     }}
-  
+
     context 'with no parameter' do
-  
-      it { should contain_class('postgis') }
-  
-      it { should contain_package('postgresql-9.1-postgis')
-        .with({
-          :ensure => :present,
-        })
-        .that_requires('Class[postgresql::server]')
-      }
+
+      it { should contain_class('postgresql::server::postgis') }
   
       it { should contain_postgresql__server__database('template_postgis')
         .with({
           :istemplate => true,
           :template   => 'template1',
         })
-        .that_requires('Package[postgresql-9.1-postgis]')
       }
   
       it { should contain_exec('createlang plpgsql template_postgis')
@@ -48,7 +40,6 @@ describe 'postgis' do
           :user   => 'postgres',
           :unless => 'echo "\dt" | psql -d template_postgis | grep -q geometry_columns',
         })
-        .that_requires('Exec[createlang plpgsql template_postgis]')
       }
   
       it { should contain_exec('psql -q -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql')
@@ -56,38 +47,53 @@ describe 'postgis' do
           :user   => 'postgres',
           :unless => 'test $(psql -At -d template_postgis -c "select count(*) from spatial_ref_sys") -ne 0',
         })
-        .that_requires('Exec[createlang plpgsql template_postgis]')
       }
+
+    end
+  end
+
+  context 'on Debian wheezy with new postgis version' do
+
+    let :pre_condition do
+      "class {'::postgresql::globals': encoding => 'UTF8', version => '9.1', postgis_version => '2.1'} -> class {'::postgresql::server':}"
+    end
+
+    let(:facts) {{
+      :concat_basedir           => '/var/lib/puppet/concat',
+      :lsbdistcodename          => 'wheezy',
+      :operatingsystem          => 'Debian',
+      :operatingsystemrelease   => 'wheezy',
+      :osfamily                 => 'Debian',
+    }}
+
+    context 'with no parameter' do
+
+      it { should contain_class('postgresql::server::postgis') }
   
-      it { should contain_postgresql__server__table_grant('GRANT ALL ON geometry_columns TO public')
+      it { should contain_postgresql__server__database('template_postgis')
         .with({
-          :privilege => 'ALL',
-          :table     => 'geometry_columns',
-          :db        => 'template_postgis',
-          :role      => 'public',
-        })
-        .that_requires('Exec[psql -q -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql]')
-        .that_notifies('Postgresql_psql[vacuum postgis]')
-      }
-  
-      it { should contain_postgresql__server__table_grant('GRANT SELECT ON spatial_ref_sys TO public')
-        .with({
-          :privilege => 'SELECT',
-          :table     => 'spatial_ref_sys',
-          :db        => 'template_postgis',
-          :role      => 'public',
-        })
-        .that_requires('Exec[psql -q -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql]')
-        .that_notifies('Postgresql_psql[vacuum postgis]')
-      }
-  
-      it { should contain_postgresql_psql('vacuum postgis')
-        .with({
-          :command     => 'VACUUM FREEZE',
-          :db          => 'template_postgis',
-          :refreshonly => true,
+          :istemplate => true,
+          :template   => 'template1',
         })
       }
+  
+      it { should contain_postgresql_psql('Add postgis extension on template_postgis')
+        .with({
+          :db      => 'template_postgis',
+          :command => 'CREATE EXTENSION postgis',
+          :unless  => "SELECT extname FROM pg_extension WHERE extname = 'postgis'",
+        })
+        .that_requires('Postgresql::Server::Database[template_postgis]')
+      }
+
+      it { should contain_postgresql_psql('Add postgis_topology extension on template_postgis')
+        .with({
+          :db      => 'template_postgis',
+          :command => 'CREATE EXTENSION postgis_topology',
+          :unless  => "SELECT extname FROM pg_extension WHERE extname = 'postgis_topology'",
+        })
+      }
+
     end
   end
 
